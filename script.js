26ef9d74e8d86f610e728d7918aa32912e756c90
JittoVarghese <119120361+JittoVarghese@users.noreply.github.com>
2024-12-29 22:40:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -33,59 +33,45 @@ faqs.forEach((faq)=>{
 // Scroll Animations
 
 
-const details = gsap.utils.toArray(".contentSection:not(:first-child)")
-const photos = gsap.utils.toArray(".desktopPhoto:not(:first-child)")
+gsap.set(".photo:not(:first-child)", {opacity:0, scale:0.5})
 
+const animation = gsap.to(".photo:not(:first-child)", {
+	opacity:1, scale:1, duration:1, stagger:1
+})
 
-gsap.set(photos, {yPercent:101})
-
-const allPhotos = gsap.utils.toArray(".desktopPhoto")
-
-
-// create
-let mm = gsap.matchMedia();
-
-// add a media query. When it matches, the associated function will run
-mm.add("(min-width: 600px)", () => {
-
-  // this setup code only runs when viewport is at least 600px wide
-  console.log("desktop")
-	
-  ScrollTrigger.create({
-	trigger:".twoside",
+ScrollTrigger.create({
+	trigger:".gallery",
 	start:"top top",
 	end:"bottom bottom",
-	pin:".contentRightSide"
+	pin:".right",
+	animation: animation,
+	scrub:true,
+	markers:true
 })
 
-//create scrolltrigger for each details section
-//trigger photo animation when headline of each details section 
-//reaches 80% of window height
-details.forEach((detail, index)=> {
-
-	let headline = detail.querySelectorAll(".lefttextheading")
-	let animation = gsap.timeline()
-	   .to(photos[index], {yPercent:0})
-	   .set(allPhotos[index], {autoAlpha:0})
-	ScrollTrigger.create({
-		trigger:headline,
-		start:"top 80%",
-		end:"top 80%",
-		animation:animation,
-		scrub:true,
-		markers:false
-	})
-})
-	
-	
-  
-  return () => { // optional
-    // custom cleanup code here (runs when it STOPS matching)
-	  console.log("mobile")
-  };
-});
 
 
+
+/* ScrollTrigger Docs
+
+https://greensock.com/docs/v3/Plugins/ScrollTrigger
+
+*/
+
+
+
+
+
+/* 
+
+learn more GreenSock and ScrollTrigger
+
+https://www.creativeCodingClub.com
+
+new lessons weekly
+less than $1 per week
+
+*/
 // Scroll Animations
 
 
