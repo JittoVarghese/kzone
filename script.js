43cfaf223cedec9cf6163e34abe97ad529d90a0d
JittoVarghese <119120361+JittoVarghese@users.noreply.github.com>
2025-01-11 15:26:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -59,88 +59,57 @@ faqs.forEach((faq)=>{
 //accordions sections ends
 
 
-
-// Scroll Animations
-
-
-// gsap.set(".photo:not(:first-child)", {opacity:0, scale:0.5})
-
-// const animation = gsap.to(".photo:not(:first-child)", {
-// 	opacity:1, scale:1, duration:1, stagger:1
-// })
-
-// ScrollTrigger.create({
-// 	trigger:".gallery",
-// 	start:"top top",
-// 	end:"bottom bottom",
-// 	pin:".right",
-// 	animation: animation,
-// 	scrub:true,
-// 	markers:true
-// })
+// scroll pinning animation
+const details = gsap.utils.toArray(".desktopContentSection:not(:first-child)")
+const photos = gsap.utils.toArray(".desktopPhoto:not(:first-child)")
 
 
+gsap.set(photos, {yPercent:101})
+
+const allPhotos = gsap.utils.toArray(".desktopPhoto")
 
 
-/* ScrollTrigger Docs
+// create
+let mm = gsap.matchMedia();
 
-https://greensock.com/docs/v3/Plugins/ScrollTrigger
+// add a media query. When it matches, the associated function will run
+mm.add("(min-width: 600px)", () => {
 
-*/
+  // this setup code only runs when viewport is at least 600px wide
+  console.log("desktop")
+	
+  ScrollTrigger.create({
+	trigger:".gallery",
+	start:"top top",
+	end:"bottom bottom",
+	pin:".right"
+})
 
+//create scrolltrigger for each details section
+//trigger photo animation when headline of each details section 
+//reaches 80% of window height
+details.forEach((detail, index)=> {
 
+	let headline = detail.querySelector("h1")
+	let animation = gsap.timeline()
+	   .to(photos[index], {yPercent:0})
+	   .set(allPhotos[index], {autoAlpha:0})
+	ScrollTrigger.create({
+		trigger:headline,
+		start:"top 80%",
+		end:"top 50%",
+		animation:animation,
+		scrub:true,
+		markers:false
+	})
+})
+	
+	
+  
+  return () => { // optional
+    // custom cleanup code here (runs when it STOPS matching)
+	  console.log("mobile")
+  };
+});
 
-
-
-/* 
-
-learn more GreenSock and ScrollTrigger
-
-https://www.creativeCodingClub.com
-
-new lessons weekly
-less than $1 per week
-
-*/
-// Scroll Animations
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-/* ScrollTrigger Docs
-
-https://greensock.com/docs/v3/Plugins/ScrollTrigger
-
-*/
-
-
-
-
-
-/* 
-
-learn more GreenSock and ScrollTrigger
-
-https://www.creativeCodingClub.com
-
-new lessons weekly
-less than $1 per week
-
-*/
+// scroll pinning animation
